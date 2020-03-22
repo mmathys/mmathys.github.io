c8de24886afd98cd1e3d0f4609f6576de15e737b
Max Mathys <threadox@gmail.com>
2020-03-22 18:41:36 +0100

    put code that worked where the code that didn't used to be

---
+++ wie-bsoffe-bisch/script.js
@@ -0,0 +1,26 @@
+mode = "max"
+
+messages = [
+    "Du bisch nüchtern, gurl", // 1
+    "immerno fasch nüchtern", // 2
+    "trink 2 shots, oder 1 bier", // 3
+    "danke bro", // 4
+    "schere stei papier um en shot", // 5
+    "mach e insta story!", // 6
+    "mach es verruckts 1 zu 10.", // 7
+    "zieh dich us! 😇", // 8
+    "leck bisch bsoffe", // 9
+    "EZ ESKALIERTS" // 10
+]
+
+$("#max").click(function() {
+    console.log("max")
+    $("#max").addClass("active")
+    $("#viola").removeClass("active")
+})
+
+$("#viola").click(function() {
+    console.log("viola")
+    $("#viola").addClass("active")
+    $("#max").removeClass("active")
+})
